7f594bb3ad67f1f374b9ec9133ec138659a891a6
Spiceinajar <89741444+Spiceinajar@users.noreply.github.com>
2023-07-10 14:07:00 -0400

    Add files via upload

--- navbar.js
+++ navbar.js
@@ -8,6 +8,8 @@ document.body.insertAdjacentHTML("beforebegin", `
     <button style="border:0; border-radius: 5px; padding: 5px; font-size: 20px; position:absolute; right:8px; top:8px;" ><img src="heart.png" alt="" width="20" height="20"> Buy me a coffee!</button>
     -->
   </div>
+
+  <div style="height: 50px;"></div>
 </head>
 
 `);
